ab978b251fa2f37ffd43e1cce27bee20246db17b
Anuj <pushpadhaanuj@gmail.com>
2023-03-28 22:25:05 +0530

    commit

--- front-end/src/pages/login/Login.jsx
+++ front-end/src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import "./style.css";
+import "./Style.css";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
